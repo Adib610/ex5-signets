2b900ab0ce3ea43b92630936894a41426e738503
Adib610 <adib.ben1234@gmail.com>
2021-04-01 07:40:41 -0400

    first commit

--- src/services/config.js
+++ src/services/config.js
@@ -5,7 +5,13 @@
 
 // Configuration Firebase (mettez-y les vôtres !)
 const firebaseConfig = {
-  
+  apiKey: "AIzaSyB4qGE3F3mY1iQI8RdxuG2IeN3IpUErJD4",
+  authDomain: "pvt-achat-react-adib.firebaseapp.com",
+  projectId: "pvt-achat-react-adib",
+  storageBucket: "pvt-achat-react-adib.appspot.com",
+  messagingSenderId: "1094983199635",
+  appId: "1:1094983199635:web:4cdd5e3c1748cf33ad5666",
+  measurementId: "G-30FWKTHH0D"
 };
 export default firebaseConfig;
 
